32aefa01dc774733893216d4a4c519957b3877cb
whysoserious <abed.sari4@gmail.com>
2025-12-02 22:18:09 +0200

    Day 2

--- day2/day2.py
+++ day2/day2.py
@@ -1,24 +1,36 @@
+def is_repeated(num_str):
+    # Try to divide the string into num_pieces equal pieces,
+    # with size ranging from 2 to len(str).
+    for num_pieces in range(2, len(num_str) + 1):
+        # If the length of the string isn't divisible by this number,
+        # we cannot divide the string into equal pieces of this size,
+        # therefore skip.
+        if len(num_str) % num_pieces != 0:
+            continue
+
+        # All pieces should be equal to numStr[0:size]
+        piece_size = len(num_str) // num_pieces
+        piece = num_str[0:piece_size]
+
+        if num_str == (piece * num_pieces):
+            return True
+
+    return False
+
 
 def main():
-    with open(
-        "/Users/whysoserious/repos/aoc/aoc25/day2/input.txt", "r"
-    ) as inputFile:
+    with open("/Users/whysoserious/repos/aoc/aoc25/day2/input.txt", "r") as inputFile:
         lines = inputFile.readlines()
 
-    ranges = lines[0].split(',').replace('\n', '')
-    print(ranges)
+    ranges = lines[0].replace("\n", "").split(",")
 
     invalid_ids = 0
     for id_range in ranges:
-        
-        for num in range(int(id_range[0]), int(id_range[1])):
+        start, end = id_range.split("-")
+        for num in range(int(start), int(end) + 1):
             num_str = str(num)
-            size = len(num_str)
-            first_half = num_str[0:size//2]
-            second_half = num_str[size//2:]
-
-            if first_half == second_half:
-                invalid_ids += 1
+            if is_repeated(num_str):
+                invalid_ids += num
 
     print(invalid_ids)
 
